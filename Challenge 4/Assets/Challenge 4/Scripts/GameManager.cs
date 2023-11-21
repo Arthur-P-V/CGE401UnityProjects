2ed6068b902384eea7c4f359e49b6357de78b3f3
Arthur-P-V <apetersonveatch@gmail.com>
2023-11-21 00:32:35 -0600

    Pushes Challenge 4

---
+++ Challenge 4/Assets/Challenge 4/Scripts/GameManager.cs
@@ -0,0 +1,68 @@
+﻿/*
+ Arthur Peterson-Veatch
+ Challenge 4
+ GameManager
+ A script to handle UI and win conditions
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+
+    public SpawnManagerX spawnManager;
+    public PlayerControllerX player;
+    public Text waveText;
+    public Text tutorialText;
+    public bool gameOver;
+    public bool won;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            tutorialText.enabled = false;
+            spawnManager.gameObject.SetActive(true);
+            waveText.enabled = true;
+            player.enabled = true;
+        }
+        if (!gameOver)
+        {
+            waveText.text = "Wave " + (spawnManager.waveCount - 1);
+        }
+        
+
+        if ((spawnManager.waveCount - 1) > 10) {
+            won = true;
+            gameOver = true;
+            
+        }
+
+        if (gameOver) {
+            spawnManager.enabled = false;
+            if (won)
+            {
+                waveText.text = "You Won!\n press R to restart!";
+            }
+            else {
+                waveText.text = "You Lose!\n Press R to restart!";
+            }
+
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+    }
+}
---
+++ Challenge 4/Assets/Challenge 4/Scripts/SpawnManagerX.cs
@@ -0,0 +1,95 @@
+﻿/*
+ Arthur Peterson-Veatch
+ Challenge 4
+ SpawnManagerX
+ A script to handle the spawning of enemies and powerups
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Analytics;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    public GameObject powerupPrefab;
+    public GameManager gameManager;
+
+    private float spawnRangeX = 10;
+    private float spawnZMin = 15; // set min spawn Z
+    private float spawnZMax = 25; // set max spawn Z
+
+    public int enemyCount;
+    public int enemyGoalHits;
+    public int playerGoalHits;
+    public int waveCount = 1;
+    public float enemySpeed = 10f;
+
+
+    public GameObject player;
+
+    void Start() {
+        enemySpeed = 10f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemyCount = GameObject.FindGameObjectsWithTag("Enemy").Length;
+
+        if (enemyCount == 0)
+        {
+            if (playerGoalHits == (waveCount - 1) && playerGoalHits != 0)
+            {
+                gameManager.gameOver = true;
+            }
+            SpawnEnemyWave(waveCount);
+        }
+
+    }
+
+    // Generate random spawn position for powerups and enemy balls
+    Vector3 GenerateSpawnPosition ()
+    {
+        float xPos = Random.Range(-spawnRangeX, spawnRangeX);
+        float zPos = Random.Range(spawnZMin, spawnZMax);
+        return new Vector3(xPos, 0, zPos);
+    }
+
+
+    void SpawnEnemyWave(int enemiesToSpawn)
+    {
+        Vector3 powerupSpawnOffset = new Vector3(0, 0, -15); // make powerups spawn at player end
+
+        // If no powerups remain, spawn a powerup
+        if (GameObject.FindGameObjectsWithTag("Powerup").Length == 0) // check that there are zero powerups
+        {
+            Instantiate(powerupPrefab, GenerateSpawnPosition() + powerupSpawnOffset, powerupPrefab.transform.rotation);
+        }
+
+        // Spawn number of enemy balls based on wave number
+        for (int i = 0; i < enemiesToSpawn; i++)
+        {
+            var enemy = Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+            enemy.GetComponent<EnemyX>().speed = enemySpeed;
+            enemy.GetComponent<EnemyX>().spawnManager = gameObject.GetComponent<SpawnManagerX>();
+        }
+        playerGoalHits = 0;
+        enemyGoalHits = 0;
+        waveCount++;
+        ResetPlayerPosition(); // put player back at start
+        enemySpeed += 2f;
+
+    }
+
+    // Move player back to position in front of own goal
+    void ResetPlayerPosition ()
+    {
+        player.transform.position = new Vector3(0, 1, -7);
+        player.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        player.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+
+    }
+
+}
